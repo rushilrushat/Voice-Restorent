74c826c62b91198180fb1b69b33bc2cb58630e47
rushilrushat <rushilrushat@gmail.com>
2021-04-29 18:31:41 +0530

    error in order place

--- app/src/main/java/com/rushil/voicerestaurant/MainActivity.kt
+++ app/src/main/java/com/rushil/voicerestaurant/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.rushil.voicerestaurant
 
 
-import android.Manifest
 import android.app.AlertDialog
 import android.app.ProgressDialog
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.speech.RecognitionListener
-import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
 import android.speech.tts.TextToSpeech
 import android.util.Log
@@ -16,7 +11,6 @@ import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.nitrico.lastadapter.LastAdapter
@@ -31,7 +25,7 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 
-class MainActivity : AppCompatActivity(), RecognitionListener {
+class MainActivity : AppCompatActivity(){
     var itemRef = FirebaseDatabase.getInstance().getReference("items")
     var TAG = "main"
     private var rvItems: RecyclerView? = null
@@ -42,95 +36,66 @@ class MainActivity : AppCompatActivity(), RecognitionListener {
     lateinit var adapter: LastAdapter
     lateinit var progressDialog: ProgressDialog
     lateinit var TtoS: TextToSpeech
-    private val permission = 100
     private lateinit var speech: SpeechRecognizer
-    private lateinit var recognizerIntent: Intent
+
+    var defaultQuestion="Hello, I am you Waiter. What do you want to order?"
+    lateinit var voiceCommand:VoiceCommand
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        voiceCommand=VoiceCommand(this)
         rvItems = findViewById(R.id.rvItem)
         addItem = findViewById(R.id.addItem)
         micButton = findViewById(R.id.micButton)
-
-        TtoS=TextToSpeech(applicationContext, TextToSpeech.OnInitListener { status ->
-            if (status != TextToSpeech.ERROR){
-                //if there is no error then set language
-                TtoS.language = Locale.US
-            }
-        })
         rvItems!!.layoutManager = LinearLayoutManager(applicationContext)
 
+        setAdapter()
+        readData()
         progressDialog = ProgressDialog(this)
         progressDialog.setMessage("Please Wait")
         progressDialog.setCancelable(false)
         progressDialog.show()
 
-        setAdapter()
-        readData()
-        voiceInit()
         micButton!!.setOnClickListener {
-            ActivityCompat.requestPermissions(this@MainActivity,
-                arrayOf(Manifest.permission.RECORD_AUDIO),
-                permission)
+           voiceCommand.speakOut(defaultQuestion)
+
         }
+        addItem!!.setOnClickListener {
+            showAlert()
+        }
+
+
+    }
+
+    private fun showAlert() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Add Item")
-        val customLayout: View = layoutInflater
-            .inflate(
-                R.layout.custom_layout,
-                null
-            )
+        val customLayout: View = layoutInflater.inflate(R.layout.custom_layout, null)
         builder.setView(customLayout)
 
-        builder
-            .setPositiveButton(
-                "Add"
-            ) { dialog, which -> // send data from the
-                val etname = customLayout.findViewById<EditText>(R.id.etName).text.toString()
-                val etprice = customLayout.findViewById<EditText>(R.id.etPrice).text.toString()
-
-                addItem(etname, etprice)
-            }
+        builder.setPositiveButton("Add")
+        { dialog, which -> // send data from the
+            val etname = customLayout.findViewById<EditText>(R.id.etName).text.toString()
+            val etprice = customLayout.findViewById<EditText>(R.id.etPrice).text.toString()
+            addItem(etname, etprice)
+        }
 
         dialog = builder.create()
-
-        addItem!!.setOnClickListener {
-
-            dialog!!.show()
-
-        }
-
-    }
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>,
-                                            grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            permission -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager
-                    .PERMISSION_GRANTED) {
-                speech.startListening(recognizerIntent)
-            } else {
-                Toast.makeText(this@MainActivity, "Permission Denied!",
-                    Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-    private fun voiceInit() {
-        speech = SpeechRecognizer.createSpeechRecognizer(this)
-        speech.setRecognitionListener(this)
-        recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "US-en")
-        recognizerIntent.putExtra(
-            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3)
+        dialog!!.show()
 
     }
 
+//    override fun onStop() {
+//        super.onStop()
+//        speech.destroy()
+//        TtoS.stop()
+//        Log.i(TAG, "destroy")
+//    }
     private fun addItem(name: String, price: String) {
         val Id: String = itemRef.push().getKey()!!
         Log.d("data=>", itemRef.toString())
-        val items = Items(Id, name, price)
+        val items = Items(Id, name, price.toInt())
         itemRef.child(Id).setValue(items)
         Toast.makeText(applicationContext, "Item addes", Toast.LENGTH_LONG).show()
     }
@@ -149,10 +114,11 @@ class MainActivity : AppCompatActivity(), RecognitionListener {
                     val name = model["name"].toString()
                     val id = model["id"].toString()
 
-                    val user = Items(id, name, price)
+                    val user = Items(id, name, price.toInt())
                     Log.d(TAG, user.id)
                     itemList.add(user)
                 }
+                voiceCommand.itemList=itemList
                 adapter.notifyDataSetChanged()
             }
 
@@ -171,82 +137,105 @@ class MainActivity : AppCompatActivity(), RecognitionListener {
                     val position = it.adapterPosition
                     it.binding.iDel.setOnClickListener {
                         itemRef.child(itemList[position].id).removeValue()
-                        Toast.makeText(
-                            this@MainActivity,
-                            itemList[position].name + " Remove",
-                            Toast.LENGTH_LONG
-                        ).show()
+                        Toast.makeText(this@MainActivity, itemList[position].name + " Remove", Toast.LENGTH_LONG).show()
 
                     }
                 }
             }.into(rvItems!!)
     }
-    override fun onStop() {
-        super.onStop()
-        speech.destroy()
-        TtoS.stop()
-        Log.i(TAG, "destroy")
-    }
-    override fun onReadyForSpeech(params: Bundle?) {
 
-    }
-
-    override fun onRmsChanged(rmsdB: Float) {
-
-    }
-
-    override fun onBufferReceived(buffer: ByteArray?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onPartialResults(partialResults: Bundle?) {
-
-    }
-
-    override fun onEvent(eventType: Int, params: Bundle?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBeginningOfSpeech() {
-        Log.i(TAG, "onBeginningOfSpeech")
-
-    }
-
-    override fun onEndOfSpeech() {
-
-    }
-
-    override fun onError(error: Int) {
-        val errorMessage: String = getErrorText(error)
-        Log.d(TAG, "FAILED $errorMessage")
-        Toast.makeText(this,errorMessage,Toast.LENGTH_LONG).show()
-        TtoS.speak(errorMessage, TextToSpeech.QUEUE_FLUSH, null)
-    }
-
-    private fun getErrorText(error: Int): String {
-        var message = ""
-        message = when (error) {
-            SpeechRecognizer.ERROR_AUDIO -> "Audio recording error"
-            SpeechRecognizer.ERROR_CLIENT -> "Client side error"
-            SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "Insufficient permissions"
-            SpeechRecognizer.ERROR_NETWORK -> "Network error"
-            SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> "Network timeout"
-            SpeechRecognizer.ERROR_NO_MATCH -> "No match"
-            SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> "RecognitionService busy"
-            SpeechRecognizer.ERROR_SERVER -> "error from server"
-            SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "No speech input"
-            else -> "Didn't understand, please try again."
-        }
-        return message
-    }
-
-    override fun onResults(results: Bundle?) {
-        val matches = results!!.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-        var text = ""
-        for (result in matches!!) text = """
-      $result
-      """.trimIndent()
-        Toast.makeText(this,text,Toast.LENGTH_LONG).show()
-        TtoS.speak(text, TextToSpeech.QUEUE_FLUSH, null)
-    }
+//
+//    private fun getErrorText(error: Int): String {
+//        var message = ""
+//        message = when (error) {
+//            SpeechRecognizer.ERROR_AUDIO -> "Audio recording error"
+//            SpeechRecognizer.ERROR_CLIENT -> "Client side error"
+//            SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "Insufficient permissions"
+//            SpeechRecognizer.ERROR_NETWORK -> "Network error"
+//            SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> "Network timeout"
+//            SpeechRecognizer.ERROR_NO_MATCH -> "No match"
+//            SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> "RecognitionService busy"
+//            SpeechRecognizer.ERROR_SERVER -> "error from server"
+//            SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "No speech input"
+//            else -> "Didn't understand, please try again."
+//        }
+//        return message
+//    }
+//
+//
+//    private fun speakOut(question: String){
+//        TtoS.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
+//            override fun onStart(s: String) {}
+//            override fun onDone(s: String) {
+//                runOnUiThread {
+//                    requestAudioPermissions()
+//                }
+//            }
+//
+//            override fun onError(s: String) {}
+//        })
+//        val params = Bundle()
+//        params.putString(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "")
+//        TtoS.speak(question, TextToSpeech.QUEUE_FLUSH, params, question)
+//        this.question = question
+//    }
+//    private fun requestAudioPermissions() {
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
+//                Toast.makeText(this, "Please grant permissions to record audio", Toast.LENGTH_LONG)
+//                    .show()
+//                //Give user option to still opt-in the permissions
+//            } else {
+//                // Show user dialog to grant permission to record audio
+//            }
+//            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), MY_PERMISSIONS_RECORD_AUDIO)
+//        } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+//            startSpeechToText()
+//        }
+//    }
+//
+//    private fun startSpeechToText() {
+//        val speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+//        val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+//        speechRecognizerIntent.putExtra(
+//            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+//            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+//        )
+//        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+//        speechRecognizer.setRecognitionListener(object : RecognitionListener {
+//            override fun onReadyForSpeech(bundle: Bundle) {}
+//            override fun onBeginningOfSpeech() {}
+//            override fun onRmsChanged(v: Float) {}
+//            override fun onBufferReceived(bytes: ByteArray) {}
+//            override fun onEndOfSpeech() {}
+//
+//            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+//            override fun onError(error: Int) {
+//                val errorMessage: String = getErrorText(error)
+//                Log.d(TAG, "FAILED $errorMessage")
+//                Toast.makeText(applicationContext, errorMessage, Toast.LENGTH_LONG).show()
+//                speakOut(errorMessage)
+//            }
+//
+//            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+//            override fun onResults(bundle: Bundle) {
+//                val matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+//                var text=""
+//                for (result in matches!!) text = """$result""".trimIndent()
+//                Answer(text)
+////                Toast.makeText(applicationContext,text,Toast.LENGTH_LONG).show()
+//            }
+//
+//            override fun onPartialResults(bundle: Bundle) {}
+//            override fun onEvent(i: Int, bundle: Bundle) {}
+//        })
+//        speechRecognizer.startListening(speechRecognizerIntent)
+//    }
+//    fun Answer(text: String) {
+//        if (answers.contains(text)){
+//            showAlert()
+//        }else{
+//            Toast.makeText(applicationContext,"Error=>"+text,Toast.LENGTH_LONG).show()
+//        }
+//    }
 }
--- app/src/main/java/com/rushil/voicerestaurant/model/Items.kt
+++ app/src/main/java/com/rushil/voicerestaurant/model/Items.kt
@@ -1,3 +1,4 @@
 package com.rushil.voicerestaurant.model
 
-data class Items(val id:String,val name:String,val price:String)
+data class Items(val id:String,val name:String,val price:Int)
+data class OrderItems(val o_id:String,val i_id:String,val i_name:String,val quintity:Int,val totalPrice:Int)
